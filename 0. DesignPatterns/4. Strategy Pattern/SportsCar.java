2656ea85b983efe43628b2fe8b0b7001464e25cf
Himanshu Singh <63532987+hs19980912@users.noreply.github.com>
2023-11-28 01:15:26 +0530

    Create SportsCar.java

---
+++ 0. DesignPatterns/4. Strategy Pattern/SportsCar.java
@@ -0,0 +1,8 @@
+public class SportsCar extends Vechile{
+
+    SportsCar() {
+        super(new SportsDriveStrategy());
+        //TODO Auto-generated constructor stub
+    }
+    // Implicit super constructor Vechile() is undefined for default constructor. Must define an explicit constructorJava(134217868)
+}
