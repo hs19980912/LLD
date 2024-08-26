ca07dab249d58c0c2c8712427ad0617943bb931d
Himanshu Singh <hs19980912@gmail.com>
2024-08-27 00:39:24 +0530

    Update 3. Liskov substitution principle.cpp

--- 1. Solid principles/3. Liskov substitution principle.cpp
+++ 1. Solid principles/3. Liskov substitution principle.cpp
@@ -2,4 +2,4 @@ Liskov substitution principle
 
 If class B is a subtype of class A, then we should be able to replace object of class A with B without breaking the behaviour of the program.
 
-Sbclass should extend the capability of the parent class and NOT narrow it down.
+Subclass should extend the capability of the parent class and NOT narrow it down.
